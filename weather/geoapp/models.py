a2ad6b6a78db2247621032cabb4c0ea1902f45dc
Thiago Teixeira <thsteixeira@gmail.com>
2025-07-28 18:34:28 -0700

    second commit

--- weather/geoapp/models.py
+++ weather/geoapp/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Continent(models.Model):
-    name = models.CharField(max_length=100)
+    continent = models.CharField(max_length=100)
     results = models.JSONField()
     search_timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name}, {self.search_timestamp.strftime('%Y-%m-%d %H:%M:%S')}, {self.results}"
+        return f"{self.continent}, {self.search_timestamp.strftime('%Y-%m-%d %H:%M:%S')}, {self.results}"
